95cfbeab396df866822db796b80d69abb55a0dc8
Josh Day <emailjoshday@gmail.com>
2016-05-13 21:07:12 -0700

    updating tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,12 @@
 using StandardizedMatrices
-using Base.Test
+using FactCheck, StatsBase
 
-# write your own tests here
-@test 1 == 1
+
+facts("Matrix-Vector multiplication") do
+	x = randn(100,100)
+	x2 = zscore(x, 1)
+	z = StandardizedMatrix(x)
+	b = randn(100)
+
+	@fact x2 * b --> roughly(z * b)
+end
